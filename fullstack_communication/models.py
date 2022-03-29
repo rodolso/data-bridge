33226bda17cb8956e7720c6f31f326c6b17ecfbc
edugonzalvez <edgoncar94@gmail.com>
2022-03-29 13:34:37 +0200

    updated the DDBB

--- fullstack_communication/models.py
+++ fullstack_communication/models.py
@@ -3,9 +3,26 @@ from django.db import models
 # Create your models here.
 
 
+class Questions(models.Model):
+    question_id = models.IntegerField(unique=True, primary_key=True)
+    question = models.CharField(max_length=140, unique=True)
+    type_response_id = models.IntegerField()
+
+    class Meta:
+        unique_together = (('question_id', 'question'),)
+
+
+class CategoryMaster(models.Model):
+    type_response = models.CharField(max_length=15, unique=True)
+    response_id = models.IntegerField(primary_key=True, unique=True)
+
+    class Meta:
+        unique_together = (('type_response', 'response_id'),)
+
+
 class Companies(models.Model):
-    company_id = models.IntegerField()
-    company_name = models.CharField(max_length=20, primary_key=True)
+    company_id = models.IntegerField(primary_key=True, unique=True)
+    company_name = models.CharField(max_length=20, unique=True)
     type_company = models.CharField(max_length=8)
 
     class Meta:
@@ -14,46 +31,39 @@ class Companies(models.Model):
 
 class Formularios(models.Model):
     employee_id = models.IntegerField()
-    question_id = models.IntegerField()
+    question_id = models.ForeignKey(Questions, to_field='question_id', on_delete=models.CASCADE)
     answer_id = models.IntegerField(null=True)
-    company_id = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    company_id = models.ForeignKey(Companies, to_field='company_id', on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now=True)
 
     class Meta:
-        unique_together = (('employee_id', 'question_id'),)
+        unique_together = (('employee_id', 'question_id', 'company_id', 'datetime'),)
 
 
 class Category(models.Model):
     category_id = models.IntegerField(primary_key=True)
-    category_name = models.CharField(max_length=30)
+    category_name = models.CharField(max_length=30, unique=True)
+
+    class Meta:
+        unique_together = (('category_id', 'category_name'),)
 
 
 class TypeResponses(models.Model):
-    type_response = models.CharField(max_length=15)
+    type_response = models.ForeignKey(CategoryMaster, to_field='type_response', on_delete=models.CASCADE)
     answer_id = models.IntegerField()
     answer_string = models.CharField(max_length=25)
     score = models.FloatField(null=True)
 
     class Meta:
-        unique_together = (('type_response', 'answer_id'),)
+        unique_together = (('type_response', 'answer_id', 'score'),)
 
 
 class Results(models.Model):
-    question_id = models.ForeignKey(Formularios, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Questions, to_field='question_id', on_delete=models.CASCADE)
     question = models.CharField(max_length=140)
-    type_company = models.CharField(max_length=3)
+    type_company = models.CharField(max_length=8)
     result = models.FloatField()
     active = models.CharField(max_length=6)
-    type_response = models.ForeignKey(TypeResponses, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = (('type_company', 'question_id'),)
-
-
-
-
-
-
-
+    type_response = models.ForeignKey(CategoryMaster, to_field='type_response', on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, to_field='category_id', on_delete=models.CASCADE)
 
--- populate_script.py
+++ populate_script.py
@@ -11,9 +11,12 @@ def insert(row: dict, model_class: ModelBase):
     """
     insert = model_class(**row)
     insert.save()
-    
+
+
 [q.delete() for q in Companies.objects.all()]
 [q.delete() for q in Formularios.objects.all()]
+[q.delete() for q in Questions.objects.all()]
+[q.delete() for q in CategoryMaster.objects.all()]
 [q.delete() for q in Category.objects.all()]
 [q.delete() for q in TypeResponses.objects.all()]
 [q.delete() for q in Results.objects.all()]
@@ -24,27 +27,42 @@ companies_dict = companies.to_dict(orient='records')
 for row in companies_dict:
     insert(row, Companies)
 
-    
+
+category = pd.read_excel('bbdd_project.xlsx', sheet_name='category')
+category_dict = category.to_dict(orient='records')
+
+for row in category_dict:
+    insert(row, Category)
+
+
+category_master = pd.read_excel('bbdd_project.xlsx', sheet_name='category_master')
+category_master_dict = category_master.to_dict(orient='records')
+
+for row in category_master_dict:
+    insert(row, CategoryMaster)
+
+
+questions = pd.read_excel('bbdd_project.xlsx', sheet_name='questions')
+questions_dict = questions.to_dict(orient='records')
+
+for row in questions_dict:
+    insert(row, Questions)
+
 formularios = pd.read_excel('bbdd_project.xlsx', sheet_name='formularios')
 formularios['datetime'] = formularios['datetime'].astype(str)
 formularios_dict = formularios.to_dict(orient='records')
 
 for row in formularios_dict:
     row['company_id'] = Companies.objects.filter(company_id=row['company_id']).first()
+    row['question_id'] = Questions.objects.filter(question_id=row['question_id']).first()
     insert(row, Formularios)
-    
-
-category = pd.read_excel('bbdd_project.xlsx', sheet_name='category')
-category_dict = category.to_dict(orient='records')
-
-for row in category_dict:
-    insert(row, Category) 
     
 
 type_responses = pd.read_excel('bbdd_project.xlsx', sheet_name='type_responses')
 type_responses_dict = type_responses.to_dict(orient='records')
 
 for row in type_responses_dict:
+    row['type_response'] = CategoryMaster.objects.filter(type_response=row['type_response']).first()
     insert(row, TypeResponses)
     
     
@@ -52,8 +70,8 @@ results = pd.read_excel('bbdd_project.xlsx', sheet_name='results')
 results_dict = results.to_dict(orient='records')
 
 for row in results_dict:
-    row['question_id'] = Formularios.objects.filter(question_id=row['question_id']).first()
-    row['type_response'] = TypeResponses.objects.filter(type_response=row['type_response']).first()
+    row['question_id'] = Questions.objects.filter(question_id=row['question_id']).first()
+    row['type_response'] = CategoryMaster.objects.filter(type_response=row['type_response']).first()
     row['category_id'] = Category.objects.filter(category_id=row['category_id']).first()
     insert(row, Results)
 
